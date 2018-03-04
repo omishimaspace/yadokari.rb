61e984664d1902edd67c13108ef38db012edad1f
nof <sakape@gmail.com>
2018-03-04 10:49:10 +0900

    fix me

--- lib/yadokari/me.rb
+++ lib/yadokari/me.rb
@@ -10,15 +10,20 @@ module Yadokari
 
     def run
       result = do_get("/me/#{@token}")
+      puts result
       puts <<-EOS
 *** YOUR INFOMATION ***
-check in date: #{result['reservation[check_in_on]']}
-check out date: #{result['reservation[check_out_on]']}
-your name: #{result['reservation[name]']}
-email address: #{result['reservation[email]']}
-tel: #{result['reservation[tel]']}
-how many man: #{result['reservation[men_number]']}
-how many women: #{result['reservation[women_number]']}
+check in date: #{result['check_in_on']}
+check out date: #{result['check_out_on']}
+your name: #{result['name']}
+email address: #{result['email']}
+tel: #{result['tel']}
+how many man: #{result['men_number']}
+how many women: #{result['women_number']}
+check in time: #{result['check_in_time']}
+purpose of use: #{result['purpose_of_use']}
+payment method: #{result['payment_method']}
+note: #{result['note']}
 **********************
       EOS
     end
